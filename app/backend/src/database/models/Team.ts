a31df500f8c46f42014749bbbc27709de6bb361e
Renato Bispo <renatosilvabispo@outlook.com>
2022-05-03 19:50:17 -0300

    Create Team model

---
+++ app/backend/src/database/models/Team.ts
@@ -0,0 +1,23 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class Team extends Model {
+  public id!: number;
+
+  public teamName!: string;
+}
+
+Team.init(
+  {
+    id: INTEGER,
+    teamName: STRING,
+  },
+  {
+    underscored: true,
+    sequelize: db,
+    timestamps: false,
+  },
+);
+
+export default Team;
