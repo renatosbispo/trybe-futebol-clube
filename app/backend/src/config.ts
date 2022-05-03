5a35379a2651abb85753ef76bc5426a0550f6be5
Renato Bispo <renatosilvabispo@outlook.com>
2022-05-03 19:31:48 -0300

    Add db config to class and compilation step to db:reset

---
+++ app/backend/src/config.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import { Options } from 'sequelize';
+
+export default class Config {
+  public static db: Options = {
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || '',
+    database: process.env.DB_NAME || 'TRYBE_FUTEBOL_CLUBE',
+    host: process.env.DB_HOST || 'localhost',
+    port: Number(process.env.DB_PORT) || 3306,
+    dialect: 'mysql',
+    dialectOptions: {
+      timezone: 'Z',
+    },
+    logging: false,
+  };
+}
--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -1,17 +1,3 @@
-import 'dotenv/config';
-import { Options } from 'sequelize';
+import Config from '../../config';
 
-const config: Options = {
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '123456',
-  database: process.env.DB_NAME || 'TRYBE_FUTEBOL_CLUBE',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3002,
-  dialect: 'mysql',
-  dialectOptions: {
-    timezone: 'Z',
-  },
-  logging: false,
-}
-
-module.exports = config;
+module.exports = Config.db;
