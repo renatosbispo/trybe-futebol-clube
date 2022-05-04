a61ba8024fd0e97167e256b041737aab737d8a5d
Renato Bispo <renatosilvabispo@outlook.com>
2022-05-04 19:36:52 -0300

    Create express app

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -2,29 +2,40 @@ import * as express from 'express';
 
 class App {
   public app: express.Express;
-  // ...
 
   constructor() {
-    // ...
+    this.app = express();
     this.config();
-    // ...
+    this.setupRoutes();
   }
 
-  private config():void {
+  protected config(): void {
     const accessControl: express.RequestHandler = (_req, res, next) => {
       res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+      res.header(
+        'Access-Control-Allow-Methods',
+        'GET,POST,DELETE,OPTIONS,PUT,PATCH',
+      );
       res.header('Access-Control-Allow-Headers', '*');
       next();
     };
 
     this.app.use(accessControl);
-    // ...
+    this.app.use(express.json());
   }
 
-  // ...
-  public start(PORT: string | number):void {
-    // ...
+  protected setupRoutes(): void {
+    this.app.get('/ping', (_req, res) => {
+      res.status(200).json({ message: 'pong' });
+    });
+  }
+
+  public start(PORT: string | number): void {
+    const port = typeof PORT === 'string' ? Number(PORT) : PORT;
+
+    this.app.listen(port, () => {
+      console.log(`Listening on port ${port}...`);
+    });
   }
 }
 
