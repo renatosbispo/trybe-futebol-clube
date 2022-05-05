fda19267494058ee6f70e81dde125143c9e2d40c
Renato Bispo <renatosilvabispo@outlook.com>
2022-05-05 18:29:17 -0300

    Write test for response code

---
+++ app/backend/src/tests/post-login.test.ts
@@ -0,0 +1,40 @@
+import * as sinon from 'sinon';
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+import { app } from '../app';
+import Example from '../database/models/Example';
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('POST /login', () => {
+  let response: Response;
+
+  // before(async () => {
+  //   sinon
+  //     .stub(Example, "findOne")
+  //     .resolves({
+  //       ...<Seu mock>
+  //     } as Example);
+  // });
+
+  // after(()=>{
+  //   (Example.findOne as sinon.SinonStub).restore();
+  // })
+
+  // it('...', async () => {
+  //   response = await chai
+  //      .request(app)
+  //      ...
+
+  //   expect(...)
+  // });
+
+  it('The response status code should be 200', async () => {
+    response = await chai.request(app).post('/login');
+
+    expect(response.status).to.equal(200);
+  });
+});
