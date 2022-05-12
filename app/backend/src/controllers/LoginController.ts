245fee2f020fb069c4aa92348bc7fe5c1b740566
Renato Bispo <renatosilvabispo@outlook.com>
2022-05-11 21:06:58 -0300

    Implement functionality to pass user data in response body test

--- app/backend/src/controllers/LoginController.ts
+++ app/backend/src/controllers/LoginController.ts
@@ -3,17 +3,38 @@ import {
   AuthenticatedUserInterface,
   UserModelInterface,
 } from '../interfaces/user';
+import { AuthService, UserService } from '../services';
 
 export default class LoginController {
+  protected authService: AuthService;
+
+  protected userService: UserService;
+
+  constructor(authService: AuthService, userService: UserService) {
+    this.authService = authService;
+    this.userService = userService;
+  }
+
   public async login(
-    _req: Request<
-      unknown,
-      AuthenticatedUserInterface,
-      Pick<UserModelInterface, 'email' | 'password'>
+    req: Request<
+    unknown,
+    AuthenticatedUserInterface,
+    Pick<UserModelInterface, 'email' | 'password'>
     >,
     res: Response<AuthenticatedUserInterface>,
-    _next: NextFunction
+    _next: NextFunction,
   ): Promise<void> {
-    res.status(200).end();
+    try {
+      const { email, password: passwordFromReq } = req.body;
+
+      await this.authService.verifyCredentials(email, passwordFromReq);
+
+      const user = await this.userService.findByEmail(email) as UserModelInterface;
+      const { password, ...userWithoutPassword } = user;
+
+      res.status(200).json({ user: userWithoutPassword, token: 'token' });
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
