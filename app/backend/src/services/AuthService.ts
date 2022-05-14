d3e62b2e1d213c35910a6e20571ced0f4631823e
Renato Bispo <renatosilvabispo@outlook.com>
2022-05-13 21:23:05 -0300

    feat: create token generation method

--- app/backend/src/services/AuthService.ts
+++ app/backend/src/services/AuthService.ts
@@ -1,9 +1,14 @@
+import jwt, { SignOptions } from 'jsonwebtoken';
 import { UserModelInterface, UserRepo } from '../interfaces/user';
+import TokenPayloadInterface from '../interfaces/auth/TokenPayload.interface';
 
 export default class AuthService {
+  protected jwtSecret: string;
+
   protected userRepo: UserRepo;
 
-  constructor(userRepo: UserRepo) {
+  constructor(userRepo: UserRepo, jwtSecret: string) {
+    this.jwtSecret = jwtSecret;
     this.userRepo = userRepo;
   }
 
@@ -16,5 +21,16 @@ export default class AuthService {
     if (!user || user.password !== password) {
       throw new Error('Incorrect email or password');
     }
+  }
+
+  public generateToken(payload: TokenPayloadInterface): string {
+    const jwtConfig: SignOptions = {
+      algorithm: 'HS256',
+      expiresIn: '12h',
+    };
+
+    const token = jwt.sign(payload, this.jwtSecret, jwtConfig);
+
+    return token;
   }
 }
