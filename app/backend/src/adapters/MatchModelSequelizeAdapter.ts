8d40ca88208d81f8c7594e6c6b7d94e1ac3978ed
Renato Bispo <renatosilvabispo@outlook.com>
2022-05-15 10:12:42 -0300

    fix: include team relationship

--- app/backend/src/adapters/MatchModelSequelizeAdapter.ts
+++ app/backend/src/adapters/MatchModelSequelizeAdapter.ts
@@ -1,4 +1,5 @@
 import MatchModel from '../database/models/MatchModel';
+import TeamModel from '../database/models/TeamModel';
 import { MatchModelInterface, MatchRepoInterface } from '../interfaces/match';
 
 export default class MatchModelSequelizeAdapter implements MatchRepoInterface {
@@ -9,7 +10,12 @@ export default class MatchModelSequelizeAdapter implements MatchRepoInterface {
   }
 
   public async findAll(): Promise<MatchModelInterface[]> {
-    const matches = await this._MatchModel.findAll();
+    const matches = await this._MatchModel.findAll({
+      include: [
+        { model: TeamModel, as: 'teamHome', attributes: ['teamName'] },
+        { model: TeamModel, as: 'teamAway', attributes: ['teamName'] },
+      ],
+    });
 
     return matches;
   }
