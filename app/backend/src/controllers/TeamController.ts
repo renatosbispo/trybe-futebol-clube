0a3dbd29d82c2a2f16e73f8722ce48400a12855d
Renato Bispo <renatosilvabispo@outlook.com>
2022-05-14 22:25:39 -0300

    feat: team controller find all method

--- app/backend/src/controllers/TeamController.ts
+++ app/backend/src/controllers/TeamController.ts
@@ -1,1 +1,25 @@
-export default class TeamController {}
+import { NextFunction, Request, Response } from 'express';
+import { TeamModelInterface } from '../interfaces/team';
+import { TeamService } from '../services';
+
+export default class TeamController {
+  protected teamService: TeamService;
+
+  constructor(teamService: TeamService) {
+    this.teamService = teamService;
+  }
+
+  public async findAll(
+    _req: Request<unknown, TeamModelInterface[]>,
+    res: Response<TeamModelInterface[]>,
+    next: NextFunction,
+  ): Promise<void> {
+    try {
+      const teams = await this.teamService.findAll();
+
+      res.status(200).json(teams);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
