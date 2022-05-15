cb33bed068ff404bec56b2111c2ef322d1a86c39
Renato Bispo <renatosilvabispo@outlook.com>
2022-05-14 22:43:05 -0300

    feat: add team router and reorganize constructor

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,11 +1,13 @@
 import express, { Express, RequestHandler } from 'express';
 import fs from 'fs';
+import TeamModelSequelizeAdapter from './adapters/TeamModelSequelizeAdapter';
 import UserModelSequelizeAdapter from './adapters/UserModelSequelizeAdapter';
-import { LoginController } from './controllers';
-import { UserRepo } from './interfaces/user';
+import { LoginController, TeamController } from './controllers';
+import { TeamRepoInterface } from './interfaces/team';
+import { UserRepoInterface } from './interfaces/user';
 import { AuthMiddleware, ErrorMiddleware } from './middlewares';
-import { LoginRouter } from './routers';
-import { AuthService, UserService } from './services';
+import { LoginRouter, TeamRouter } from './routers';
+import { AuthService, TeamService, UserService } from './services';
 
 class App {
   public app: Express;
@@ -20,28 +22,33 @@ class App {
 
   protected loginRouter: LoginRouter;
 
+  protected teamController: TeamController;
+
+  protected teamRepo: TeamRepoInterface;
+
+  protected teamRouter: TeamRouter;
+
+  protected teamService: TeamService;
+
+  protected userRepo: UserRepoInterface;
+
   protected userService: UserService;
 
-  protected userRepo: UserRepo;
-
   constructor() {
-    this.jwtSecret = fs.readFileSync('jwt.evaluation.key', { encoding: 'utf-8' });
-    this.userRepo = new UserModelSequelizeAdapter();
-
-    this.authService = new AuthService(this.userRepo, this.jwtSecret);
-    this.userService = new UserService(this.userRepo);
-
-    this.loginController = new LoginController(
-      this.authService,
-      this.userService,
-    );
-
-    this.authMiddleware = new AuthMiddleware(this.authService);
-    this.loginRouter = new LoginRouter(this.authMiddleware, this.loginController);
+    this.jwtSecret = fs.readFileSync('jwt.evaluation.key', {
+      encoding: 'utf-8',
+    });
 
     this.app = express();
     this.config();
+
+    this.setupRepos();
+    this.setupServices();
+    this.setupControllers();
+    this.setupMiddlewares();
+    this.setupRouters();
     this.setupRoutes();
+
     this.app.use(ErrorMiddleware.handleError);
   }
 
@@ -60,12 +67,42 @@ class App {
     this.app.use(express.json());
   }
 
-  protected setupRoutes(): void {
-    this.app.get('/ping', (_req, res) => {
-      res.status(200).json({ message: 'pong' });
-    });
+  protected setupControllers(): void {
+    this.loginController = new LoginController(
+      this.authService,
+      this.userService,
+    );
 
+    this.teamController = new TeamController(this.teamService);
+  }
+
+  protected setupServices(): void {
+    this.authService = new AuthService(this.userRepo, this.jwtSecret);
+    this.teamService = new TeamService(this.teamRepo);
+    this.userService = new UserService(this.userRepo);
+  }
+
+  protected setupMiddlewares(): void {
+    this.authMiddleware = new AuthMiddleware(this.authService);
+  }
+
+  protected setupRepos(): void {
+    this.teamRepo = new TeamModelSequelizeAdapter();
+    this.userRepo = new UserModelSequelizeAdapter();
+  }
+
+  protected setupRoutes(): void {
     this.app.use('/login', this.loginRouter.router);
+    this.app.use('/teams', this.teamRouter.router);
+  }
+
+  protected setupRouters(): void {
+    this.loginRouter = new LoginRouter(
+      this.authMiddleware,
+      this.loginController,
+    );
+
+    this.teamRouter = new TeamRouter(this.teamController);
   }
 
   public start(PORT: string | number): void {
