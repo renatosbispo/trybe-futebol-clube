c9879f4c1385a1a2fc8f5c7e2ceab765ee315d91
Renato Bispo <renatosilvabispo@outlook.com>
2022-05-14 23:09:51 -0300

    fix: missing null in typing

--- app/backend/src/interfaces/team/TeamRepo.interface.ts
+++ app/backend/src/interfaces/team/TeamRepo.interface.ts
@@ -2,5 +2,5 @@ import TeamModelInterface from './TeamModel.interface';
 
 export default interface TeamRepoInterface {
   findAll(): Promise<TeamModelInterface[]>;
-  findById(id: number): Promise<TeamModelInterface>;
+  findById(id: number): Promise<TeamModelInterface | null>;
 }
