78283ec5f401301f16ccf9291a88898c6a1f5964
Renato Bispo <renatosilvabispo@outlook.com>
2022-05-23 02:07:05 -0300

    feat: sort homeLeaderboard

--- app/backend/src/services/LeaderboardService.ts
+++ app/backend/src/services/LeaderboardService.ts
@@ -1,8 +1,11 @@
+import { firstBy } from 'thenby';
 import LeaderboardInterface from '../interfaces/leaderboard/Leaderboard.interface';
 import { MatchRepoInterface } from '../interfaces/match';
 import TeamService from './TeamService';
 
 export default class LeaderboardService {
+  protected homeLeaderboard: LeaderboardInterface[];
+
   protected matchRepo: MatchRepoInterface;
 
   protected teamService: TeamService;
@@ -35,14 +38,18 @@ export default class LeaderboardService {
     return totalHomeDraws + totalHomeVictories * 3;
   }
 
-  public async getLeaderboardHome(): Promise<LeaderboardInterface[]> {
+  public async getHomeLeaderboard(): Promise<LeaderboardInterface[]> {
     const teams = await this.teamService.findAll();
 
     const leaderboard = await Promise.all(
       teams.map(({ id }) => this.getTeamStatsHome(id)),
     );
 
-    return leaderboard;
+    this.homeLeaderboard = leaderboard;
+
+    this.sortHomeLeaderboard();
+
+    return this.homeLeaderboard;
   }
 
   public async getTeamStatsHome(id: number): Promise<LeaderboardInterface> {
@@ -58,5 +65,26 @@ export default class LeaderboardService {
       goalsBalance: await this.getHomeGoalsBalance(id),
       efficiency: await this.getHomeEfficiency(id),
     };
+  }
+
+  public async sortHomeLeaderboard() {
+    this.homeLeaderboard.sort(
+      firstBy(
+        (a: LeaderboardInterface, b: LeaderboardInterface) =>
+          b.totalVictories - a.totalVictories,
+      )
+        .thenBy(
+          (a: LeaderboardInterface, b: LeaderboardInterface) =>
+            b.goalsBalance - a.goalsBalance,
+        )
+        .thenBy(
+          (a: LeaderboardInterface, b: LeaderboardInterface) =>
+            b.goalsFavor - a.goalsFavor,
+        )
+        .thenBy(
+          (a: LeaderboardInterface, b: LeaderboardInterface) =>
+            a.goalsOwn - b.goalsOwn,
+        ),
+    );
   }
 }
