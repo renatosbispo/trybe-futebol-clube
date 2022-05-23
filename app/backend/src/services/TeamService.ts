2aede9af7aab4f65470af4dee97e01fe2fb6682e
Renato Bispo <renatosilvabispo@outlook.com>
2022-05-23 00:51:48 -0300

    feat: getGoalsFor method

--- app/backend/src/services/TeamService.ts
+++ app/backend/src/services/TeamService.ts
@@ -14,13 +14,13 @@ export default class TeamService {
 
   public async countHomeGamesWhere(
     id: number,
-    predicate: (match: MatchModelInterface) => boolean,
+    predicate: (match: MatchModelInterface) => boolean
   ): Promise<number> {
     const matches = await this.findHomeMatches(id);
 
     const totalHomeMatches = matches.reduce(
       (total, match) => (predicate(match) ? total + 1 : total),
-      0,
+      0
     );
 
     return totalHomeMatches;
@@ -32,13 +32,21 @@ export default class TeamService {
     return teams;
   }
 
+  public async findAwayMatches(id: number): Promise<MatchModelInterface[]> {
+    const matches = await this.matchRepo.findAll();
+
+    const awayMatches = matches.filter(({ awayTeam }) => awayTeam === id, 0);
+
+    return awayMatches;
+  }
+
   public async findById(id: string): Promise<TeamModelInterface> {
     const team = await this.teamRepo.findById(Number(id));
 
     if (!team) {
       throw new ErrorWithCode(
         'ENTITY_NOT_FOUND',
-        `Team with id ${id} not found`,
+        `Team with id ${id} not found`
       );
     }
 
@@ -53,24 +61,53 @@ export default class TeamService {
     return homeMatches;
   }
 
+  public async getAwayGoalsFor(id: number): Promise<number> {
+    const awayMatches = await this.findHomeMatches(id);
+
+    const awayGoalsFor = awayMatches.reduce(
+      (total, { awayTeamGoals }) => total + awayTeamGoals,
+      0,
+    );
+
+    return awayGoalsFor;
+  }
+
+  public async getGoalsFor(id: number): Promise<number> {
+    const awayGoalsFor = await this.getAwayGoalsFor(id);
+    const homeGoalsFor = await this.getHomeGoalsFor(id);
+
+    return awayGoalsFor + homeGoalsFor;
+  }
+
+  public async getHomeGoalsFor(id: number): Promise<number> {
+    const homeMatches = await this.findHomeMatches(id);
+
+    const homeGoalsFor = homeMatches.reduce(
+      (total, { homeTeamGoals }) => total + homeTeamGoals,
+      0,
+    );
+
+    return homeGoalsFor;
+  }
+
   public async getHomeDraws(id: number): Promise<number> {
     return this.countHomeGamesWhere(
       id,
-      ({ homeTeamGoals, awayTeamGoals }) => homeTeamGoals === awayTeamGoals,
+      ({ homeTeamGoals, awayTeamGoals }) => homeTeamGoals === awayTeamGoals
     );
   }
 
   public async getHomeLosses(id: number): Promise<number> {
     return this.countHomeGamesWhere(
       id,
-      ({ homeTeamGoals, awayTeamGoals }) => homeTeamGoals < awayTeamGoals,
+      ({ homeTeamGoals, awayTeamGoals }) => homeTeamGoals < awayTeamGoals
     );
   }
 
   public async getHomeVictories(id: number): Promise<number> {
     return this.countHomeGamesWhere(
       id,
-      ({ homeTeamGoals, awayTeamGoals }) => homeTeamGoals > awayTeamGoals,
+      ({ homeTeamGoals, awayTeamGoals }) => homeTeamGoals > awayTeamGoals
     );
   }
 }
