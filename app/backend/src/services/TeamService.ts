47c9319e0e9a4198e2edc87beb25607b1327516d
Renato Bispo <renatosilvabispo@outlook.com>
2022-05-23 00:13:39 -0300

    feat: create method for finding games

--- app/backend/src/services/TeamService.ts
+++ app/backend/src/services/TeamService.ts
@@ -1,10 +1,14 @@
 import ErrorWithCode from '../lib/error-with-code';
 import { TeamModelInterface, TeamRepoInterface } from '../interfaces/team';
+import { MatchModelInterface, MatchRepoInterface } from '../interfaces/match';
 
 export default class TeamService {
+  protected matchRepo: MatchRepoInterface;
+
   protected teamRepo: TeamRepoInterface;
 
-  constructor(teamRepo: TeamRepoInterface) {
+  constructor(teamRepo: TeamRepoInterface, matchRepo: MatchRepoInterface) {
+    this.matchRepo = matchRepo;
     this.teamRepo = teamRepo;
   }
 
@@ -25,5 +29,15 @@ export default class TeamService {
     }
 
     return team;
+  }
+
+  public async findGames(id: number): Promise<MatchModelInterface[]> {
+    const matches = await this.matchRepo.findAll();
+
+    const totalGames = matches.filter(({ homeTeam, awayTeam }) => (
+      awayTeam === id || homeTeam === id
+    ), 0);
+
+    return totalGames;
   }
 }
