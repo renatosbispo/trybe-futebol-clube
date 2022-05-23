ca27b18ae1140a886d9f925d5bc53b343276e8c0
Renato Bispo <renatosilvabispo@outlook.com>
2022-05-23 04:51:05 -0300

    feat: away leaderboard controller method

--- app/backend/src/controllers/LeaderboardController.ts
+++ app/backend/src/controllers/LeaderboardController.ts
@@ -1,10 +1,16 @@
 import { NextFunction, Request, Response } from 'express';
-import { HomeLeaderboardService } from '../services';
+import { AwayLeaderboardService, HomeLeaderboardService } from '../services';
 
 export default class LeaderboardController {
+  protected awayLeaderboardService: AwayLeaderboardService;
+
   protected homeLeaderboardService: HomeLeaderboardService;
 
-  constructor(homeLeaderboardService: HomeLeaderboardService) {
+  constructor(
+    homeLeaderboardService: HomeLeaderboardService,
+    awayLeaderboardService: AwayLeaderboardService,
+  ) {
+    this.awayLeaderboardService = awayLeaderboardService;
     this.homeLeaderboardService = homeLeaderboardService;
   }
 
@@ -17,6 +23,20 @@ export default class LeaderboardController {
       const homeLeaderboard = await this.homeLeaderboardService.getHomeLeaderboard();
 
       res.status(200).json(homeLeaderboard);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  public async getAwayLeaderboard(
+    _req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> {
+    try {
+      const awayLeaderboard = await this.awayLeaderboardService.getAwayLeaderboard();
+
+      res.status(200).json(awayLeaderboard);
     } catch (error) {
       next(error);
     }
